c1502d422f96450c716894cec18fbb6569a9eacc
Jenna <xujenna@gmail.com>
2018-03-03 22:07:08 -0500

    updated productivity script

--- trackers/rescuetime/getproductivity.py
+++ trackers/rescuetime/getproductivity.py
@@ -1,19 +1,31 @@
 import requests
 import json
+import webbrowser
+import time
+import datetime
 
-# get updated json from rescue time
-r = requests.get(url='https://www.rescuetime.com/anapi/data?key=B63X_PtvpSERKQwozOFH_6oXpXwlXTcWh3c3OxfE&perspective=interval&restrict_kind=efficiency&interval=hour&restrict_begin=2018-02-15&restrict_end=2018-03-02&format=json')
 
-data = r.json()
+def checkProductivity():
+    dt = datetime.datetime.now()
+    currentDate = str(dt.year) + "-" + str(dt.month) + "-" + str(dt.day)
+    url = "https://www.rescuetime.com/anapi/data?key=B63X_PtvpSERKQwozOFH_6oXpXwlXTcWh3c3OxfE&perspective=interval&restrict_kind=efficiency&interval=hour&restrict_begin=2018-02-15&restrict_end=" + currentDate + "&format=json"
+    # get updated json from rescue time
+    r = requests.get(url=url)
 
-#save json to file
-with open('productivity.json', 'w') as f:
-    json.dump(data, f, indent=4, sort_keys=True)
+    data = r.json()
 
-rows = data['rows']
+    #save json to file
+    with open('productivity.json', 'w') as f:
+        json.dump(data, f, indent=4)
 
-efficiency = rows[-1][4]
+    # isolate "Efficiency (percent)" value of last array: rows > index number (by hour) > 4
+    rows = data['rows']
+    efficiency = rows[-1][4]
+    print(efficiency)
+    # if efficiency value is less than 50, open xujenna.com/focus
+    if efficiency < 50:
+        webbrowser.open_new('http://www.xujenna.com/focus')
 
-print(efficiency)
-# isolate "Efficiency (percent)" value of last array: rows > index number (by hour) > 4
-# if efficiency value is less than 50, open xujenna.com/focus
+while True:
+    checkProductivity()
+    time.sleep(3600)
