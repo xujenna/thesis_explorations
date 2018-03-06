75ce6200efd3321a23c0209625c02113d325b711
Jenna <xujenna@gmail.com>
2018-03-06 17:52:07 -0500

    updated viz, cleaned up mindwandering data

--- trackers/rescuetime/getproductivity.py
+++ trackers/rescuetime/getproductivity.py
@@ -8,7 +8,7 @@ import datetime
 def checkProductivity():
     dt = datetime.datetime.now()
     currentDate = str(dt.year) + "-" + str(dt.month) + "-" + str(dt.day)
-    url = "https://www.rescuetime.com/anapi/data?key=B63X_PtvpSERKQwozOFH_6oXpXwlXTcWh3c3OxfE&perspective=interval&restrict_kind=efficiency&interval=hour&restrict_begin=2018-02-15&restrict_end=" + currentDate + "&format=json"
+    url = "https://www.rescuetime.com/anapi/data?key=B63X_PtvpSERKQwozOFH_6oXpXwlXTcWh3c3OxfE&perspective=interval&restrict_kind=efficiency&interval=hour&restrict_begin=2018-03-01&restrict_end=" + currentDate + "&format=json"
     # get updated json from rescue time
     r = requests.get(url=url)
 
@@ -16,7 +16,7 @@ def checkProductivity():
 
     #save json to file
     with open('productivity.json', 'w') as f:
-        json.dump(data, f, indent=4)
+        json.dump(data, f, indent=4, sort_keys=True)
 
     # isolate "Efficiency (percent)" value of last array: rows > index number (by hour) > 4
     rows = data['rows']
