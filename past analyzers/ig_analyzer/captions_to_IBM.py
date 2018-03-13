ff618660aa2a43ecee8257684b9676a6bf135e4a
Jenna <xujenna@gmail.com>
2018-03-13 13:57:08 -0400

    caption sentiment analysis, updated ig analyzer viz

--- past analyzers/ig_analyzer/captions_to_IBM.py
+++ past analyzers/ig_analyzer/captions_to_IBM.py
@@ -9,19 +9,28 @@ tone_analyzer = ToneAnalyzerV3(
 )
 
 
+with open('captionsToIBM.json', 'w+') as f:
+    f.write("[\n")
 
 captions = [];
 with open('ig_to_clarifai.json', 'r') as IGjson:
     clarifaiData = json.load(IGjson)
     for elem in clarifaiData:
-        currentCaption = elem["caption"]
-        currentDate = elem["date"]
-        holder = {}
-        tone = tone_analyzer.tone(currentCaption, content_type="text/plain;charset=utf-8")
-        holder["date"] = currentDate
-        holder["tones"] = tone["sentences_tone"]
-        captions.append(holder)
-        break
+        if(len(elem["caption"]) > 0):
+            currentCaption = elem["caption"]
+            currentDate = elem["date"]
+            holder = {}
+            tone = tone_analyzer.tone(currentCaption, content_type="text/plain;charset=utf-8")
+            if isinstance(tone, dict) and "sentences_tone" in tone:
+                holder["date"] = currentDate
+                holder["tones"] = tone["sentences_tone"]
+                captions.append(holder)
+                print(captions)
+                with open('captionsToIBM.json', 'a') as f:
+                    f.write(json.dumps(captions, indent=2) + ",\n")
 
-with open('captionsToIBM.json', 'w+') as f:
-	json.dump(captions, f, indent=2)
+with open('captionsToIBM.json', 'a') as f:
+    f.write("]\n")
+
+# with open('captionsToIBM.json', 'w+') as f:
+# 	json.dump(captions, f, indent=2)
