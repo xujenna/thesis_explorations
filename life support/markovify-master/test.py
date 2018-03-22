331f6047d67ace0a2aecc910e3a5eeaa795d5eac
Jenna <xujenna@gmail.com>
2018-03-22 16:43:52 -0400

    added sugar corpus

--- life support/markovify-master/test.py
+++ life support/markovify-master/test.py
@@ -7,17 +7,19 @@ with open("../corpuses/horoscope.txt") as f:
     horoscope = f.read()
 with open("../corpuses/oprah.txt") as f:
     oprah = f.read()
-
+with open("../corpuses/sugar.txt") as f:
+    sugar = f.read()
 
 # Build the model.
 ggiaModel = markovify.Text(ggia)
 horoscopeModel = markovify.Text(horoscope)
 oprahModel = markovify.Text(oprah)
-comboModel = markovify.combine([ggiaModel, horoscopeModel, oprahModel], [1.5, 1, 1])
+sugarModel = markovify.Text(sugar)
+comboModel = markovify.combine([ggiaModel, horoscopeModel, oprahModel], [2, 1, 1])
 #
 # for i in range(2):
-#     print("\n"+text_model.make_sentence()+"\n")
+#     print("\n"+comboModel.make_sentence()+"\n")
 
 # Print three randomly-generated sentences of no more than 140 characters
-for i in range(2):
+for i in range(1):
     print("\n"+comboModel.make_short_sentence(140)+"\n")
