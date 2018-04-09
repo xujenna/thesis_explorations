340728d265b694db440bdf9e87ee6ccd927ab99a
Jenna <xujenna@gmail.com>
2018-04-09 11:53:54 -0400

    updated reporter

--- trackers/reporter/hourly_qs.py
+++ trackers/reporter/hourly_qs.py
@@ -4,12 +4,11 @@ import time
 import os
 
 def writeToCSV(response):
-    with open('responses.csv', 'a') as f:
-        writer = csv.writer(f)
+    with open('responses.tsv', 'a') as f:
+        writer = csv.writer(f, delimiter='\t')
         writer.writerow(response)
 
 while True:
-    time.sleep(5)
     os.system("osascript -e \'display notification \"Questionnaire Time!\" with title \"Questionnaire Time!\" sound name \"Pop\"\' -e \'activate application \"Terminal\"\'")
 
     dateTimeNum = datetime.datetime.now().timestamp()
@@ -17,22 +16,16 @@ while True:
     mood = input("Mood 1-5: ")
     moodNotes = input("Mood notes: ")
     trigger = input("Trigger: ")
-    activity = input("Activities: ")
+    activity = input("Activities (Schoolwork/InClass/Leisure/Errands/Eating/Cooking/Reading/Resting/Chatting/Email/WithFriends): ")
+    social = input("# of Unique Interactions: ")
+    alone = input("Alone (True/False): ")
+    location = input("Location: ")
     morale = input("Morale 1-5: ")
     stress = input("Stress 1-5: ")
     fatigue = input("Fatigue 1-5: ")
-    compulsions = input("Compulsion: ")
+    compulsions = input("Compulsion (True/False): ")
 
-    response = []
-    response.append(timestamp)
-    response.append(mood)
-    response.append(moodNotes)
-    response.append(trigger)
-    response.append(activity)
-    response.append(morale)
-    response.append(stress)
-    response.append(fatigue)
-    response.append(compulsions)
+    response = [timestamp, mood, moodNotes, trigger, activity, social, alone, location, morale, stress, fatigue, compulsions]
 
     writeToCSV(response)
     print("\nRecorded responses at " + timestamp + "\n")
