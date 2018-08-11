547798e0b913fa1751704b7b456c0a1f8d2a62b4
Jenna <xujenna@gmail.com>
2018-08-11 18:06:03 -0500

    added QualityTime data to correlation

--- past analyzers/ig_analyzer/getIGsrcs.py
+++ past analyzers/ig_analyzer/getIGsrcs.py
@@ -4,14 +4,16 @@ from bs4 import BeautifulSoup
 import csv
 import time
 import datetime
+from http.client import IncompleteRead
 
-page_url = "https://www.ins2tagram.com/53964/"
+page_url = "https://www.instagram.com/53964/"
 
 def request_until_succeed(url):
     req = urllib.request.Request(url)
     success = False
     while success is False:
         try:
+            time.sleep(5)
             response = urllib.request.urlopen(req)
             if response.getcode() == 200:
                 success = True
@@ -20,12 +22,27 @@ def request_until_succeed(url):
             time.sleep(5)
             print("Error fur URL {}: {}".format(url, datetime.datetime.now()))
             print("retrying.")
+        except IncompleteRead:
+            time.sleep(5)
+             # Oh well, reconnect and keep trucking
+            continue
+        # except SocketError as e:
+        #     errorcode = e[0]
+        #     time.sleep(5)
+        #     if errorcode!=errno.ECONNREFUSED:
+        #         print("There is a time-out")
+        #         # Not the error we are looking for, re-raise
+        #         raise e
+
     return response.read().decode('utf-8')
 
-def get_more_posts(page_url, end_cursor):
-    url = page_url + '?max_id=' + end_cursor
+def get_more_posts(page_url, end_cursor, my_id):
+    url = "https://instagram.com/graphql/query/?query_id=17888483320059182&id=" + my_id + "&first=12&after=" + end_cursor
+    print("url")
+    print(url)
+
     page_source = request_until_succeed(url)
-    page = BeautifulSoup(page_source)
+    page = BeautifulSoup(page_source,"lxml")
     scripts = page.find_all('script')
     for script in scripts:
         if script.text[:18] == "window._sharedData":
@@ -34,37 +51,40 @@ def get_more_posts(page_url, end_cursor):
     return json_data
 
 def process_post(json_data):
-    owner_id = json_data['owner']['id']
-    post_id = json_data['id']
-    display_src = json_data['display_src']
-    media_preview = json_data['media_preview']
-    comments_count = json_data['comments']['count']
-    gating_info = json_data['gating_info']
+    owner_id = json_data['node']['owner']['id']
+    post_id = json_data['node']['id']
+    display_src = json_data['node']['display_url']
+    media_preview = json_data['node']['media_preview']
+    comments_count = json_data['node']['edge_media_to_comment']['count']
+    gating_info = json_data['node']['gating_info']
     caption = ''
     try:
-        caption = json_data['caption']
+        caption = json_data['node']['edge_media_to_caption']['edges']['node']['text']
     except:
         pass
-    thumbnail_src = json_data['thumbnail_src']
-    comments_disabled = json_data['comments_disabled']
-    likes_count = json_data['likes']['count']
-    is_video = json_data['is_video']
-    dimensions = json_data['dimensions']
-    typename = json_data['__typename']
-    code = json_data['code']
+    thumbnail_src = json_data['node']['thumbnail_src']
+    comments_disabled = json_data['node']['comments_disabled']
+    likes_count = json_data['node']['edge_media_preview_like']['count']
+    is_video = json_data['node']['is_video']
+    dimensions = json_data['node']['dimensions']
+    typename = json_data['node']['__typename']
+    code = json_data['node']['shortcode']
     link = 'https://www.instagram.com/p/' + code
     video_views = ''
     try:
-        video_views = json_data['video_views']
+        video_views = json_data['node']['video_views']
     except:
         pass
-    date = json_data['date']
+    date = json_data['node']['taken_at_timestamp']
     date = str(datetime.datetime.utcfromtimestamp(date).year)+'-'+str(datetime.datetime.utcfromtimestamp(date).month)+'-'+str(datetime.datetime.utcfromtimestamp(date).day)+' '+str(datetime.datetime.utcfromtimestamp(date).hour)+':'+str(datetime.datetime.utcfromtimestamp(date).minute)+':'+str(datetime.datetime.utcfromtimestamp(date).second)
     date = datetime.datetime.strptime(date,'%Y-%m-%d %H:%M:%S')
     date = date + datetime.timedelta(hours=-5) #EST
     return(owner_id, post_id, display_src, media_preview, comments_count, gating_info, caption, thumbnail_src, comments_disabled, likes_count, is_video, dimensions, typename, link, video_views, date)
 
 def scrape_instagram(page_url):
+    print("page_url")
+    print(page_url)
+
     scrape_starttime = datetime.datetime.now()
     page_source = request_until_succeed(page_url)
     page = BeautifulSoup(page_source)
@@ -73,27 +93,30 @@ def scrape_instagram(page_url):
         if script.text[:18] == "window._sharedData":
             break
     json_data = json.loads(script.contents[0][21:-1])
+    # print(json_data)
     has_next_page = True
-    num_posts = json_data['entry_data']['ProfilePage'][0]['user']['media']['count']
-    username = json_data['entry_data']['ProfilePage'][0]['user']['username']
+    num_posts = json_data['entry_data']['ProfilePage'][0]['graphql']['user']['edge_owner_to_timeline_media']['count']
+    username = json_data['entry_data']['ProfilePage'][0]['graphql']['user']['username']
     num_processed = 0
     print("Scraping {}'s {} Instagram Posts: {}\n".format(username, num_posts, scrape_starttime))
-    with open('C:\\Users\\xhargrav\\Desktop\\{}_Instagram.csv'.format(username),'w', newline='', encoding='utf-8') as file:
+    with open('{}_Instagram.csv'.format(username),'w', newline='', encoding='utf-8') as file:
         csv.writer(file).writerow(["owner_id", "post_id", "display_src", \
             "media_preview", "comments_count", "gating_info", "caption", \
             "thumbnail_src", "comments_disabled", "likes_count", "is_video", \
             "dimensions", "typename", "code", "video_views", "date"])
         while has_next_page:
-            for i in range(0,len(json_data['entry_data']['ProfilePage'][0]['user']['media']['nodes'])):
-                post_to_process = json_data['entry_data']['ProfilePage'][0]['user']['media']['nodes'][i]
+            for i in range(0,len(json_data['entry_data']['ProfilePage'][0]['graphql']['user']['edge_owner_to_timeline_media']['edges'])):
+                post_to_process = json_data['entry_data']['ProfilePage'][0]['graphql']['user']['edge_owner_to_timeline_media']['edges'][i]
                 csv.writer(file).writerow(process_post(post_to_process))
                 num_processed += 1
                 if num_processed % 100 == 0:
                     print("{} Statuses Processed: {}".format(num_processed, datetime.datetime.now()))
-            has_next_page = json_data['entry_data']['ProfilePage'][0]['user']['media']['page_info']['has_next_page']
+            has_next_page = json_data['entry_data']['ProfilePage'][0]['graphql']['user']['edge_owner_to_timeline_media']['page_info']['has_next_page']
             if has_next_page == True:
-                end_cursor = json_data['entry_data']['ProfilePage'][0]['user']['media']['page_info']['end_cursor']
-                json_data = get_more_posts(page_url, end_cursor)
+                end_cursor = json_data['entry_data']['ProfilePage'][0]['graphql']['user']['edge_owner_to_timeline_media']['page_info']['end_cursor']
+                my_id = json_data['entry_data']['ProfilePage'][0]['graphql']['user']['id']
+                json_data = get_more_posts(page_url, end_cursor, my_id)
+
             else:
                 has_next_page = False
     file.close()
