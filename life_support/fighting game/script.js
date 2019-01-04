67fea3dfc184d19b6614bc09c8547c7adb925686
Jenna Xu <jxu2@Jennas-MacBook-Pro-2.local>
2019-01-03 21:07:22 -0600

    added all fighting game content

--- life_support/fighting game/script.js
+++ life_support/fighting game/script.js
@@ -14,8 +14,9 @@ let stepContent = {};
 
 stepContent[0] = {};
 stepContent[0]["playerNum"] = 0;
-stepContent[0]["transcript"] = "Please take a comfortable seat at the same level. [[slnc 1000]] Take a moment to establish which teammate had the original complaint."
+stepContent[0]["transcript"] = ["Please take a comfortable seat at the same level. [[slnc 1000]] Take a moment to establish which teammate had the original complaint."]
 // stepContent[1]["transcript"] = "Please take a comfortable seat at the same level, maintain open body language, and face each other. [[slnc 1000]] Take a moment to establish which teammate had the original complaint. They are player one. [[slnc 1000]] Now close your eyes.[[slnc 2000]] We'll start with a breathing ritual. [[slnc 2500]] Breathe in for one, [[slnc 1000]] two, [[slnc 1000]] three, [[slnc 1000]] four. [[slnc 1000]] Back out for one [[slnc 1000]], two [[slnc 1000]], three [[slnc 1000]], four [[slnc 1000]]. In for one, [[slnc 1000]] two, [[slnc 1000]] three, [[slnc 1000]] four. [[slnc 1000]] Out for one [[slnc 1000]], two [[slnc 1000]], three [[slnc 1000]], four [[slnc 1000]]. In for one, [[slnc 1000]] two, [[slnc 1000]] three, [[slnc 1000]] four. [[slnc 1000]] Out for one [[slnc 1000]], two [[slnc 1000]], three [[slnc 1000]], four [[slnc 3000]]. You'll only get out alive if you communicate honestly and listen carefully, without assumptions. [[slnc 1500]] Focus on the challenge at hand; you will lose points for global statements, as well as excessive anger. [[slnc 1500]] The quest cannot be completed without the full cooperation of two people, so maintain a positive, generous, and compassionate attitude. [[slnc 1500]] Remember, there can only be either two winners, or two losers, so leave your ego and pride with me before you go. [[slnc 1500]] When a player completes their turn, let me know by saying [[slnc 500]] I'm done. [[slnc 3000]] Now open your eyes. Player one, let me know when you're ready, and we can begin."
+stepContent[0]["nextSubstep"] = []
 stepContent[0]["bonuses"] = []
 
 stepContent[1] = {};
@@ -55,7 +56,22 @@ stepContent[6]["nextSubstep"] = []
 stepContent[6]["bonuses"] = []
 
 
-let deEscalationScript = "Let's take a break to calm down and collect our thoughts. [[slnc 1000]] We'll start with a breathing exercise. Please close your eyes. [[slnc 2000]] Breathe in for one, [[slnc 1000]] two, [[slnc 1000]] three, [[slnc 1000]] four. [[slnc 1000]] Back out for one [[slnc 1000]], two [[slnc 1000]], three [[slnc 1000]], four [[slnc 1000]]. In for one, [[slnc 1000]] two, [[slnc 1000]] three, [[slnc 1000]] four. [[slnc 1000]] Out for one [[slnc 1000]], two [[slnc 1000]], three [[slnc 1000]], four [[slnc 1000]]. In for one, [[slnc 1000]] two, [[slnc 1000]] three, [[slnc 1000]] four. [[slnc 1000]] Out for one [[slnc 1000]], two [[slnc 1000]], three [[slnc 1000]], four [[slnc 3000]]. Remember that you are on the same team, playing for the same goal of a healthy, harmonious relationship. [[slnc 2000]] Let go of your pride, or feelings of being wronged, and consider your partner's pain. [[slnc 4000]] What can you both do to heal each other's pain and protect your relationship? [[slnc 5000]] When you're ready, open your eyes, and try to continue the conversation with one of the following phrases."
+
+let deEscalationScript = "Let's take a break to calm down and collect our thoughts. [[slnc 1000]] We'll start with a breathing exercise. Please close your eyes. [[slnc 2000]] Breathe in for one, [[slnc 1000]] two, [[slnc 1000]] three, [[slnc 1000]] four. [[slnc 1000]] Back out for one [[slnc 1000]], two [[slnc 1000]], three [[slnc 1000]], four [[slnc 1000]]. In for one, [[slnc 1000]] two, [[slnc 1000]] three, [[slnc 1000]] four. [[slnc 1000]] Out for one [[slnc 1000]], two [[slnc 1000]], three [[slnc 1000]], four [[slnc 1000]]. In for one, [[slnc 1000]] two, [[slnc 1000]] three, [[slnc 1000]] four. [[slnc 1000]] Out for one [[slnc 1000]], two [[slnc 1000]], three [[slnc 1000]], four [[slnc 3000]]. Remember that you are on the same team, playing for the same goal of a healthy, harmonious relationship. [[slnc 2000]] Let go of your pride, or feelings of being wronged, and consider your partner's pain. [[slnc 4000]] What can you both do to heal each other's pain and protect your relationship? [[slnc 5000]] When you're ready, open your eyes, and try to continue the conversation with one of the following phrases on the screen."
+
+let deEscalationCard = document.getElementById("de-escalation");
+
+deEscalationCard.onclick = function(){
+    let deEscalationScreen = document.getElementById("de-escalationScreen");
+    deEscalationScreen.style.display="block";
+    deEscalationScreen.scrollIntoView({behavior:"smooth", block:"center"});
+    speak(deEscalationScript);
+}
+
+let continueButton = document.getElementById("deEscalate-continue");
+continueButton.onClick = function(){
+    nextStep();
+}
 
 let scores = {};
 scores["player1"] = {};
@@ -103,11 +119,13 @@ function listen(){
             for (var i = event.resultIndex; i < event.results.length; ++i) {
                 console.log(event.results[i][0].transcript);
                 if(new RegExp(absolutistWords.join("|")).test(event.results[i][0].transcript)){
-                    scores["player" + stepContent[1]["playerNum"]]["penalties"] += 10;
+                    scores["player" + stepContent[turns]["playerNum"]]["penalties"] += 10;
+                    scores["player" + stepContent[turns]["playerNum"]]["score"] -= 10;
                     speak("Focus on the issue at hand; avoid using global statements.")
                 }
-                if(new RegExp(stepContent[turns]["bonuses"].join("|")).test(event.results[i][0].transcript.toLowerCase())){
-                    scores["player" + stepContent[1]["playerNum"]]["bonusPts"] += 10;
+                if(stepContent[turns]["bonuses"].length > 0 && new RegExp(stepContent[turns]["bonuses"].join("|")).test(event.results[i][0].transcript.toLowerCase())){
+                    scores["player" + stepContent[turns]["playerNum"]]["bonusPts"] += 10;
+                    scores["player" + stepContent[turns]["playerNum"]]["score"] += 10;
                 }
                 if (event.results[i].isFinal) {
                     recognition.stop();
@@ -122,11 +140,14 @@ function listen(){
 
         if(newString.includes("i'm ready", "i'm done", "next step", "continue")){
             recognition.stop();
+            if(stepContent[turns]["playerNum"] > 0){
+                scores["player" + stepContent[turns]["playerNum"]]["score"] += 10;
+            }
             turns += 1;
             transcriptIndex = 0;
             nextStep();
         }
-        else if(new RegExp(stepContent[turns]["nextSubstep"].join("|")).test(newString)){
+        else if(stepContent[turns]["nextSubstep"].length > 0 && new RegExp(stepContent[turns]["nextSubstep"].join("|")).test(newString)){
             transcriptIndex += 1;
             nextStep();
         }
@@ -147,6 +168,17 @@ function listen(){
 }
 
 function nextStep(){
+
+    if(turns==0){
+        let bonusCard = document.getElementById("bonus");
+        let deEscalationCard = document.getElementById("de-escalation");
+        let scoreCard = document.getElementById("score");
+        
+        bonusCard.style.display = "block";
+        deEscalationCard.style.display = "block";
+        scoreCard.style.display = "block";
+    }
+
     let currentStep = document.getElementById("step"+turns+"_"+transcriptIndex);
     currentStep.style.display="block";
     currentStep.scrollIntoView({behavior:"smooth", block:"center"});
